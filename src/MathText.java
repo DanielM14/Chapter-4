ef3fee3e38996689d9f3b6a8c9c02ba11a412785
Daniel McGlasson <dmcglasson90@gmail.com>
2019-02-05 11:19:25 -0600

    update

--- src/MathText.java
+++ src/MathText.java
@@ -13,6 +13,14 @@ public class MathText {
 
         System.out.println("The square root of " + number1 + "is : " + result);
 
-    }
+        
+        double personsNumber = 300;
+        
+        System.out.print("Now enter a number to find the cosine: " + personsNumber);
+        double number2 = scanner.nextDouble();
 
+        double result2 = Math.cos(number2);
+
+        System.out.println("The Cosine of " + number2 + "is " + result2);
+    }
 }
