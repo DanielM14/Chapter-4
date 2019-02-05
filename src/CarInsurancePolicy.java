18190872eb85bc94f99b3fc40007f8664121c3e7
Daniel McGlasson <dmcglasson90@gmail.com>
2019-02-05 10:05:05 -0600

    update

--- src/CarInsurancePolicy.java
+++ src/CarInsurancePolicy.java
@@ -1,32 +1,28 @@
-public class CarInsurancePolicy {
+public class CarInsurancePolicy2 {
     private int policyNumber;
     private int numPayments;
     private String residentCity;
 
-    public CarInsurancePolicy(int num, int payments, String city)
+    public CarInsurancePolicy2(int num, int payments, String city)
     {
         policyNumber = num;
         numPayments = payments;
         residentCity = city;
     }
 
-    public CarInsurancePolicy(int num, int payments)
+    public CarInsurancePolicy2(int num, int payments)
     {
-        policyNumber = num;
-        numPayments = payments;
-        residentCity = "Mayfield";
+        this(num, payments, "Mayfield");
     }
 
-    public CarInsurancePolicy(int num)
+    public CarInsurancePolicy2(int num)
     {
-        policyNumber = num;
-        numPayments = 2;
-        residentCity = "Mayfield";
+        this(num, 2, "Mayfield");
     }
 
     public void display()
     {
-        System.out.println("Policy #" + policyNumber + ". " + numPayments " payments annually. Driver resides in " + residentCity + ".");
+        System.out.println("Policy #" + policyNumber + ". " + numPayments +" payments annually. Driver resides in " + residentCity + ".");
     }
 
 }
---
+++ src/TestDogs.java
@@ -0,0 +1,9 @@
+public class TestDogs {
+    public static void main(String[] args)
+    {
+        DogTriathlonParticipant dog1 = new DogTriathlonParticipant("Bowser", 2, 85,89, 0 );
+        dog1.display();
+//        DogTriathlonParticipant dog2 = new DogTriathlonParticipant();
+//        DogTriathlonParticipant dog3 = new DogTriathlonParticipant();
+    }
+}
